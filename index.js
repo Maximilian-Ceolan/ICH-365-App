ce9a8ddb41ef7037a00d49cf9ec815dad452155c
Maximilian-Ceolan <106358160+Maximilian-Ceolan@users.noreply.github.com>
2022-05-27 13:49:27 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,14 @@
 export const applyCustomCode = (externalCodeSetup: any) => {
 	// call custom code api here
+	const onCourseSeeAllClick = (category) => {
+        props.navigation.dispatch(
+            NavigationActions.navigate({
+                routeName: "CoursesCategorySingleScreen",
+                params: {
+                    title: category.name,
+                    categoryId: category.id
+                }
+            })
+        );
+    };
 };
